76402dafa6c2fcbf7c990eaab50869d5c6ef7ddc
initofa <mus2108000@gmail.com>
2025-02-27 17:03:01 +0700

    menambahkan pesan berhasil

---
+++ berhasil.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="w3.css">
+    <link rel="icon" type="image/png" href="jujurmart.png"> 
+    <title>Berhasil</title>
+    <style>
+        body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+            background-color: #fff;
+            font-family: Arial, sans-serif;
+        }
+        .container {
+            background-color: #4CAF50;
+            padding: 15px;
+            border-radius: 15px;
+            text-align: center;
+            margin: 0 10px;
+            width: 100%;
+            max-width: 450px;
+            box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.2);
+        }
+        .message {
+            font-size: 26px;
+            font-weight: bold;
+            color: black;
+            padding: 20px;
+            background-color: white;
+            border-radius: 10px;
+            line-height: 1.5;
+        }
+        .button-container {
+            margin-top: 20px;
+            background-color: #4CAF50;
+            padding: 20px;
+            border-radius: 8px;
+        }
+        .button {
+            background-color: yellow;
+            padding: 12px;
+            border-radius: 8px;
+            display: block;
+            text-decoration: none;
+            color: black;
+            font-weight: bold;
+            text-align: center;
+            font-size: 18px;
+            width: 80%;
+            margin: auto;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div class="message">
+            Ubur - Ubur<br>
+            Ikan Lele<br>
+            Terima Kasih<br>
+            Atas Kejujurannya<br>
+            Lee
+        </div>
+        <div class="button-container">
+            <a href="penjualan.php" class="button">Kembali</a>
+        </div>
+    </div>
+</body>
+</html>
